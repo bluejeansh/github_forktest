f583a3dc680be5196de77120cc69e5c13c114397
me <bluejeansh@hanmail.net>
2022-04-07 09:31:51 +0900

    1 팀장 commit

---
+++ github_forktest/src/main/java/com/example/demo/GithubForktestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubForktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubForktestApplication.class, args);
+		System.out.println("깃허브 포크해보기");
+	}
+
+}
