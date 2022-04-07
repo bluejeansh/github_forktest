c4046a745df7727a2ac41450c54bb474ffb37586
Seongil-Yoon <dbs3346@gmail.com>
2022-04-07 10:05:05 +0900

    1.1

--- src/main/java/com/example/demo/GithubForktestApplication.java
+++ src/main/java/com/example/demo/GithubForktestApplication.java
@@ -9,6 +9,7 @@ public class GithubForktestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubForktestApplication.class, args);
 		System.out.println("깃허브 포크해보기");
+		System.out.println("깃허브 포크해보기2");
 	}
 
 }
